e8590cd6d0095943d416998de1c8d2488de3bde7
Dninja <hi@danielsnell.ninja>
2017-12-11 17:25:16 -0500

    client setup

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+
+class About extends Component {
+
+
+render() {
+
+return (
+      <div>
+
+      <h1> This is the about page </h1>
+
+       </div>
+      );
+    }
+  }
+
+export default About;
